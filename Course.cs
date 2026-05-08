3099abb9354c8d2c36eb058327e10ffb87a8880b
DATOXIC <daturbiz@gmail.com>
2026-05-08 13:23:18 +0700

    s

--- Course.cs
+++ Course.cs
@@ -6,5 +6,15 @@ namespace window_app
 {
     internal class Course
     {
+        public string Mamh { get; set; } // Mã môn học [cite: 69]
+        public string Tenmh { get; set; } // Tên môn học [cite: 69]
+        public int Sotc { get; set; } // Số tín chỉ [cite: 69]
+        public int Tuan { get; set; } // Số tuần học [cite: 69]
+        public int Hocky { get; set; } // Học kỳ [cite: 69]
+        public string Decription { get; set; } // Mô tả [cite: 69]
+
+        public bool addCourse() { return true; }
+        public bool editCourse() { return true; }
+        public bool delCourse() { return true; }
     }
 }
--- Student.cs
+++ Student.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace window_app
 {
     internal class Student
     {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public DateTime Bdate { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public MemoryStream Picture { get; set; }
+
+        // Các phương thức nghiệp vụ theo lộ trình [cite: 67]
+        public bool insertStudent() { /* Logic sẽ thêm sau */ return true; }
+        public bool updateStudent() { /* Logic Tuần 04 [cite: 19] */ return true; }
+        public bool deleteStudent() { /* Logic Tuần 04 [cite: 19] */ return true; }
+        public DataTable getStudents() { return new DataTable(); }
     }
 }
