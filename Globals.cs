65dedd7119f8ad10d0ba11e3ce426e721a65f61e
DATOXIC <daturbiz@gmail.com>
2026-05-08 22:46:56 +0700

    Update Globals.cs

--- Globals.cs
+++ Globals.cs
@@ -4,13 +4,34 @@ using System.Text;
 
 namespace window_app
 {
+    public enum UserRole
+    {
+        Admin = 0,
+        Student = 1,
+        HR = 2
+    }
     internal class Globals
     {
         public static int GlobalUserId { get; private set; }
+        public static string GlobalUsername { get; private set; }
+        public static UserRole GlobalRole { get; private set; }
+        public static string GlobalStudentID { get; private set; }
 
-        public static void SetGlobalUserId(int userId)
+        // Hàm thiết lập thông tin khi đăng nhập thành công
+        public static void SetLoginSession(int userId, string username, int role, string studentID)
         {
             GlobalUserId = userId;
+            GlobalUsername = username;
+            GlobalRole = (UserRole)role; // Ép kiểu từ int trong DB sang Enum
+            GlobalStudentID = studentID;
+        }
+
+        // Hàm xóa thông tin khi đăng xuất
+        public static void Logout()
+        {
+            GlobalUserId = 0;
+            GlobalUsername = null;
+            GlobalStudentID = null;
         }
     }
 }
