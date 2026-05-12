d5904669bfdc51e98776f80d1b24205214d866a9
nphi <phit5406@gmail.com>
2026-05-12 22:13:08 +0700

    oop hóa teacher.cs

--- Teacher.cs
+++ Teacher.cs
@@ -8,12 +8,12 @@ namespace window_app
     // Teacher.cs
     internal class Teacher : Person
     {
-        myDB db = new myDB();
+        private readonly myDB db = new myDB();
 
         // Thuộc tính riêng của giảng viên
-        public string TeacherID { get; set; }
-        public string Department { get; set; }
-        public string BaseSalary { get; set; }
+        public string TeacherID { get; private set; }
+        public string Department { get; private set; }
+        public string BaseSalary { get; private set; }
         // Hàm Insert riêng cho Teacher (Lưu vào bảng Teacher trong DB)
         public bool InsertTeacher(string tID, string fname, string lname, string dept, string email, string phone)
         {
@@ -28,18 +28,20 @@ namespace window_app
             string sql = "INSERT INTO Teacher (TeacherID, Fname, Lname, Department, Email, Phone) " +
                          "VALUES (@id, @fn, @ln, @dept, @email, @phone)";
 
-            SqlCommand command = new SqlCommand(sql, db.getConnection());
-            command.Parameters.AddWithValue("@id", this.TeacherID);
-            command.Parameters.AddWithValue("@fn", this.Fname);
-            command.Parameters.AddWithValue("@ln", this.Lname);
-            command.Parameters.AddWithValue("@dept", this.Department);
-            command.Parameters.AddWithValue("@email", this.Email);
-            command.Parameters.AddWithValue("@phone", this.Phone);
+            using (SqlCommand command = new SqlCommand(sql, db.getConnection()))
+            {
+                command.Parameters.AddWithValue("@id", this.TeacherID);
+                command.Parameters.AddWithValue("@fn", this.Fname);
+                command.Parameters.AddWithValue("@ln", this.Lname);
+                command.Parameters.AddWithValue("@dept", this.Department);
+                command.Parameters.AddWithValue("@email", this.Email);
+                command.Parameters.AddWithValue("@phone", this.Phone);
 
-            db.openConnection();
-            bool result = command.ExecuteNonQuery() == 1;
-            db.closeConnection();
-            return result;
+                db.openConnection();
+                bool result = command.ExecuteNonQuery() == 1;
+                db.closeConnection();
+                return result;
+            }
         }
     }
 }
